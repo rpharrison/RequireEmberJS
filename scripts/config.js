eb5e9488882df5d9ea66642a361921270ff9313e
Richard Harrison <admin@local.host>
2014-01-31 22:05:50 +0100

    Update Ember library to 1.3.1
    Added Ember 1.3.1 with production (also minified), debug versions and
    updated config.js.

--- scripts/config.js
+++ scripts/config.js
@@ -15,7 +15,9 @@ define({
 		/*libs*/
 		'jquery': 'libs/jquery/1.9.1/jquery',
 		'handlebars': 'libs/handlebars/1.0.rc.3/handlebars',
-		'ember': 'libs/ember/1.0.0-rc.3/ember',
+		/*'ember.dev': 'libs/ember/1.3.1/ember.dev',*/
+		/*'ember.prod': 'libs/ember/1.3.1/ember.prod',*/
+		'ember.min': 'libs/ember/1.3.1/ember.prod.min',
 		/*requirejs-plugins*/
 		'text': 'libs/requirejs-plugins/text',
 		'hbs': 'libs/requirejs-plugins/hbs',
