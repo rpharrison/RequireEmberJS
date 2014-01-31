4b02337764ee14044cdf3b29c6b688f4cb8b19d5
Richard Harrison <admin@local.host>
2014-01-31 22:28:11 +0100

    Update Handlebars to 1.3.0
    Added both versions.

--- scripts/config.js
+++ scripts/config.js
@@ -17,7 +17,8 @@ define({
 		/*'jquery': 'libs/jquery/1.11.0/jquery.min',*/
 		/*'jquery2.dev': 'libs/jquery/2.1.0/jquery.dev',*/
 		'jquery2': 'libs/jquery/2.1.0/jquery.min',
-		'handlebars': 'libs/handlebars/1.0.rc.3/handlebars',
+		/*'handlebars.dev': 'libs/handlebars/1.3.0/handlebars.dev',*/
+		'handlebars': 'libs/handlebars/1.3.0/handlebars',
 		/*'ember.dev': 'libs/ember/1.3.1/ember.dev',*/
 		/*'ember.prod': 'libs/ember/1.3.1/ember.prod',*/
 		'ember.min': 'libs/ember/1.3.1/ember.prod.min',
