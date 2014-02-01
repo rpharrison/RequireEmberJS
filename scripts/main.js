d36d4a275fc3c032b2948925b161e13416e792cd
Richard Harrison <richard@richardharrison.es>
2014-02-01 19:57:10 +0100

    Add Ember-Facebook Mixin.
    Added the Ember-Facebook Mixin from:https://github.com/luan/ember-facebook.

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,8 @@
 		requirejs.config(config);
 		require(["App", "ember.min"], function(App, Ember){
 			var app_name = config.app_name || "App";
-			root[app_name] = App = Ember.Application.create(App);
+			root[app_name] = App = Ember.Application.createWithMixins(App, Em.Facebook);
+			App.set('appId', '');
 		});
 	});
 })(this);
